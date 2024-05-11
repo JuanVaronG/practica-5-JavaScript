41395c2961d56a4c1d3daf8e12843ab47fa10116
JuanVaronG <juanchoavid_57@hotmail.com>
2024-05-10 20:53:04 -0500

    primer commit JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+
+console.log("hola mundo")
+console.log("aqui podemos escribir cualquier cosa")
+console.log("adios")
+console.log("no hay autocompletado")
+console.log("12345676")
+console.log("-1.123")
+
+//tipos de datos
+/* declarar una variable */
+let nombre ="juan varon"
+console.log(nombre)
