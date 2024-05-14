1370d563dc594546a95bc7affd592393c3d34880
JuanVaronG <juanchoavid_57@hotmail.com>
2024-05-14 18:12:21 -0500

    agregar faltante

--- script.js
+++ script.js
@@ -9,4 +9,11 @@ console.log("-1.123")
 //tipos de datos
 /* declarar una variable */
 let nombre ="juan varon"
+CSSCounterStyleRule,
 console.log(nombre)
+
+let Numero1 = 7;
+let numero2 = 9;
+
+let resultado = Numero1 + numero2
+console.log("resultado:",resultado)
